f76558673f6edde255284be4a9ce481a695f110b
Surajit Sutradhar <surajit_ug_23@ee.nits.ac.in>
2025-06-12 10:07:19 +0530

    fixed deployment bugs

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+const NotFound = () => {
+    return (
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <h2 className="text-xl font-medium">
+                Oops! Page not found.
+            </h2>
+            <Button asChild>
+                <Link href="/documents">
+                    Go back
+                </Link>
+            </Button>
+        </div>
+    )
+}
+
+export default NotFound; 
